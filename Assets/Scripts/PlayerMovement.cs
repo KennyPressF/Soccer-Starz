2a33826d83f5437ccccf0d8ffa6086449e069909
Michael Kenny <106249795+KennyPressF@users.noreply.github.com>
2023-05-21 02:02:11 +0100

    Setup input system and basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    Vector3 moveDirection;
+    Vector2 moveInput;
+    float moveDirX;
+    float moveDirZ;
+    Rigidbody rb;
+
+    GameObject ball;
+    [SerializeField] GameObject ballPoint;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue input)
+    {
+        moveInput = input.Get<Vector2>();
+        moveDirection.x = moveInput.x;
+        moveDirection.z = moveInput.y;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Ball"))
+        {
+            ball = collision.gameObject;
+            ball.transform.SetParent(ballPoint.transform);
+            ball.transform.localPosition = Vector3.zero;
+        }
+    }
+}
